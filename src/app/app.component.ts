48bbff9baf3ce1f3044d380e97cf3426063cc0e6
Karthikeyan S <RB00235@rheinbrucke.loc>
2020-03-05 20:19:50 +0530

    modified package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
 
   ngOnInit() {
     // Simple GET request with response type <any>
-    this.http.get('http://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=72d03ea8b55f41cb83d5c34eafe01069').subscribe(data => {
+    this.http.get('https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=72d03ea8b55f41cb83d5c34eafe01069').subscribe(data => {
       //debugger
       this.newsData = data;
     })
