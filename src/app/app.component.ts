d88d02936717caadaf1c9a9cc533fe2c0764e8b7
Karthikeyan S <RB00235@rheinbrucke.loc>
2020-03-05 19:36:44 +0530

    updates to deploy to heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hotNews';
+  title = 'hotNews edited';
+  newsData: any = {};
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    // Simple GET request with response type <any>
+    this.http.get('http://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=72d03ea8b55f41cb83d5c34eafe01069').subscribe(data => {
+      //debugger
+      this.newsData = data;
+    })
+  }
 }
